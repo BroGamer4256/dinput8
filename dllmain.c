56ed2221f33193e2634b23dfd1758d9700ba7c04
BroGamer <64546358+BroGamer4256@users.noreply.github.com>
2022-03-09 15:34:22 +1300

    Clean up code

--- dllmain.c
+++ dllmain.c
@@ -12,15 +12,6 @@ fn_DllGetClassObject _DllGetClassObject;
 fn_DllRegisterServer _DllRegisterServer;
 fn_DllUnregisterServer _DllUnregisterServer;
 
-void LoadOriginalLibrary(HMODULE dll)
-{
-	_DllCanUnloadNow = (fn_DllCanUnloadNow)GetProcAddress(dll, "DllCanUnloadNow");
-	_DllGetClassObject = (fn_DllGetClassObject)GetProcAddress(dll, "DllGetClassObject");
-	_DllRegisterServer = (fn_DllRegisterServer)GetProcAddress(dll, "DllRegisterServer");
-	_DllUnregisterServer = (fn_DllUnregisterServer)GetProcAddress(dll, "DllUnregisterServer");
-	_DirectInput8Create = (fn_DirectInput8Create)GetProcAddress(dll, "DirectInput8Create");
-}
-
 #ifdef __cplusplus
 extern "C" {
 #endif
@@ -39,8 +30,14 @@ STDAPI DllGetClassObject(REFCLSID rclsid, REFIID riid, LPVOID *ppv) { return _Dl
 }
 #endif
 
-void Init()
+char inited = 0;
+BOOL WINAPI DllMain(HMODULE mod, DWORD cause, void *ctx)
 {
+	if (cause != DLL_PROCESS_ATTACH || inited == 1) {
+		return TRUE;
+	}
+	inited = 1;
+
 	char buf[MAX_PATH];
 	PWSTR szSystemPath;
 
@@ -52,55 +49,35 @@ void Init()
 	wcstombs(buf, szSystemPath, MAX_PATH);
 	strcat(buf, "\\dinput8.dll");
 
-	LoadOriginalLibrary(LoadLibraryA(buf));
-}
+	HMODULE original = LoadLibraryA(buf);
+	_DllCanUnloadNow = (fn_DllCanUnloadNow)GetProcAddress(original, "DllCanUnloadNow");
+	_DllGetClassObject = (fn_DllGetClassObject)GetProcAddress(original, "DllGetClassObject");
+	_DllRegisterServer = (fn_DllRegisterServer)GetProcAddress(original, "DllRegisterServer");
+	_DllUnregisterServer = (fn_DllUnregisterServer)GetProcAddress(original, "DllUnregisterServer");
+	_DirectInput8Create = (fn_DirectInput8Create)GetProcAddress(original, "DirectInput8Create");
 
-void Load()
-{
 	wchar_t baseDir[MAX_PATH];
 	GetModuleFileNameW(0, baseDir, MAX_PATH);
 	*wcsrchr(baseDir, '\\') = 0;
 	SetCurrentDirectoryW(baseDir);
 
-	wchar_t dir[MAX_PATH];
-	wcscpy(dir, baseDir);
-	wcscat(dir, L"\\plugins\\");
-	SetCurrentDirectoryW(dir);
-
 	WIN32_FIND_DATAW fd;
-	HANDLE file = FindFirstFileW(L"*.dll", &fd);
-	if (file == INVALID_HANDLE_VALUE) return;
+	HANDLE file = FindFirstFileW(L"plugins\\*.dll", &fd);
+	if (file == INVALID_HANDLE_VALUE) return TRUE;
 
 	do
 	{
 		if (fd.dwFileAttributes & FILE_ATTRIBUTE_DIRECTORY) continue;
 		wchar_t filePath[MAX_PATH];
-		wcscpy(filePath, dir);
-
-		wchar_t* dot = wcsrchr(fd.cFileName, '.');
-		if (dot && !wcscmp(dot, L".dll"))
+		wcscpy(filePath, baseDir);
+		wcscat(filePath, L"\\plugins\\");
+		wcscat(filePath, fd.cFileName);
+		if (LoadLibraryW(filePath) == 0)
 		{
-			wcscat(filePath, fd.cFileName);
-			if (LoadLibraryW(filePath) == 0)
-			{
-				char buf[MAX_PATH];
-				wcstombs(buf, fd.cFileName, MAX_PATH);
-				MessageBoxA(0, strcat((char*)"Failed to load ", buf), "", MB_ICONERROR);
-			}
+			wcstombs(buf, fd.cFileName, MAX_PATH);
+			MessageBoxA(0, strcat((char*)"Failed to load ", buf), "", MB_ICONERROR);
 		}
 	} while (FindNextFileW(file, &fd));
-	SetCurrentDirectoryW(baseDir);
-}
-
-_Bool inited = 0;
-BOOL WINAPI DllMain(HMODULE mod, DWORD cause, void *ctx)
-{
-	if (cause != DLL_PROCESS_ATTACH || inited) {
-		return TRUE;
-	}
-	inited = 1;
-	Init();
-	Load();
 
 	return TRUE;
 }
